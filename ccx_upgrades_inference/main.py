1e1ec553bf15bf65c955e9580fddc342c6b6775a
Juan Diaz Suarez <jdiazsua@redhat.com>
2023-07-07 14:12:08 +0200

    add setup_watchtower function in order to send logs to cloudwatch

--- ccx_upgrades_inference/main.py
+++ ccx_upgrades_inference/main.py
@@ -4,9 +4,11 @@ from fastapi import FastAPI
 
 from ccx_upgrades_inference.models import UpgradeApiResponse, UpgradeRisksPredictors
 from ccx_upgrades_inference.inference import StaticPredictor
+from ccx_upgrades_inference.logging_utils import setup_watchtower
 
 from prometheus_fastapi_instrumentator import Instrumentator
 
+setup_watchtower()
 app = FastAPI()
 static_predictor = StaticPredictor()
 
